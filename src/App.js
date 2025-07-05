d75704bb21771617050bee76b766fcf65d356dde
PHF369 <social@zafaribuild.com>
2025-07-05 21:56:10 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚗 Park Hassle Free</h1>
+      <p>This is a working minimal React app on Replit.</p>
+    </div>
+  );
+}
+
+export default App;
